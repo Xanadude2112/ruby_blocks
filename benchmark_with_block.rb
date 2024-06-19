fbad9344b3c50ba6dd3a1fd2e5ab4ec5a13ceebe
Thomas Azran <tommy.azran@gmail.com>
2024-06-19 21:26:53 +0000

    ruby is hella confusing

--- benchmark_with_block.rb
+++ benchmark_with_block.rb
@@ -1,5 +1,9 @@
 def benchmark
-  # Your benchmarking code goes here.
+  start_time = Time.now
+  yield  # This will run the block of code passed to the method
+  end_time = Time.now
+  running_time = end_time - start_time  # Calculate the time difference
+  return running_time  # Return the running time
 end
  
 # Be careful, pasting this into IRB will take a long time to print.
